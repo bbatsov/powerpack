14317423ae32472e27711ad20153fac8cbc45c30
Bozhidar Batsov <bozhidar@tradeo.com>
2013-08-26 15:34:23 +0300

    Fix sproduct doc comment

--- lib/powerpack/enumerable/sproduct.rb
+++ lib/powerpack/enumerable/sproduct.rb
@@ -1,12 +1,13 @@
 unless Enumerable.method_defined? :sproduct
   module Enumerable
     # Multiplies the elements of an enum by invoking their * method.
-    # Most useful for multiplying numbers.
+    # Most useful for multiplying numbers. sproduct stands for standard product,
+    # since Ruby defines Array#product as a Carthesian product.
     #
     # @example
-    #   [1, 2, 3, 4].product #=> 6
-    #   [].product #=> nil
-    #   [].product(1) #=> 1
+    #   [1, 2, 3, 4].sproduct #=> 6
+    #   [].sproduct #=> nil
+    #   [].sproduct(1) #=> 1
     def sproduct(default = nil)
       reduce(&:*) || default
     end
