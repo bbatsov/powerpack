b64cce09a2d4b0ccffd5bb5713e294e91038867b
Bozhidar Batsov <bozhidar@tradeo.com>
2013-09-04 12:08:06 +0300

    Update take_last_while example

--- lib/powerpack/enumerable/take_last_while.rb
+++ lib/powerpack/enumerable/take_last_while.rb
@@ -5,7 +5,7 @@ unless Enumerable.method_defined? :take_last_while?
     # @return [Array] an array containing the matching elements
     #
     # @example
-    #   [1, 2, 3, 5].take_last_while(&:odd?) #=> [5, 3]
+    #   [1, 2, 3, 5].take_last_while(&:odd?) #=> [3, 5]
     def take_last_while
       return to_enum(:take_last_while) unless block_given?
 
