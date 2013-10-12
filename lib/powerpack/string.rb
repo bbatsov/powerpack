dbc1b9c75790b2db545a16ee91c4f26a50482420
Genadi Samokovarov <gsamokovarov@gmail.com>
2013-10-13 01:02:44 +0300

    Add String#remove{,!}

--- lib/powerpack/string.rb
+++ lib/powerpack/string.rb
@@ -1,6 +1,7 @@
 require_relative 'string/ascii_only'
 require_relative 'string/blank'
 require_relative 'string/format'
+require_relative 'string/remove'
 require_relative 'string/strip_indent'
 require_relative 'string/strip_margin'
 require_relative 'string/squish'
