bf8ec5336ce57c3a743408ebd42e99eb6c7d2b10
chocolateboy <chocolate@cpan.org>
2018-04-01 20:13:55 +0100

    Update Enumerable#sum to match the builtin (#35)

--- lib/powerpack/enumerable/sum.rb
+++ lib/powerpack/enumerable/sum.rb
@@ -3,18 +3,23 @@ unless Enumerable.method_defined? :sum
     # Sums up elements of a collection by invoking their `+` method.
     # Most useful for summing up numbers.
     #
-    # @param default [Object] an optional default return value if there are no elements.
-    #   It's nil by default.
-    # @return The sum of the elements or the default value if there are no
-    #   elements.
+    # @param initial [Object] an optional initial value.
+    #   It defaults to 0 for an empty collection.
+    # @return The sum of the elements, or the initial value if there
+    #   are no elements.
     #
     # @example
     #   [1, 2, 3].sum #=> 6
-    #   ["a", "b", "c"].sum #=> "abc"
     #   [[1], [2], [3]].sum #=> [1, 2, 3]
     #   [].sum #=> 0
-    def sum
-      reduce(&:+) || 0
+    #   ["a"].sum #=> "a"
+    #   ["b", "c"].sum("a") #=> "abc"
+    def sum(initial = nil)
+      if initial
+        reduce(initial, &:+)
+      else
+        reduce(&:+) || 0
+      end
     end
   end
 end
--- spec/powerpack/enumerable/sum_spec.rb
+++ spec/powerpack/enumerable/sum_spec.rb
@@ -10,11 +10,19 @@ unless Enumerable.method_defined? :sum
       expect([].sum).to be_zero
     end
 
-    it 'returns combined string' do
+    it 'returns the initial value when invoked on an empty collection' do
+      expect([].sum('')).to be_empty
+    end
+
+    it 'prepends the initial value to the sum' do
+      expect(['b', 'c'].sum('a')).to eq('abc')
+    end
+
+    it 'concatenates strings' do
       expect(['a', 'b', 'c'].sum).to eq('abc')
     end
 
-    it 'returns flatten array' do
+    it 'concatenates arrays' do
       expect([[1], [2], [3]].sum).to eq([1, 2, 3])
     end
   end
